5093764c3e7b170f3599f471de425001bede004d
craigels <craig.hanenburg@brsk.co.uk>
2025-01-16 20:36:19 +0700

    duplicate entries in dB template_data

--- route_planner/scripts/import_truckstops.py
+++ route_planner/scripts/import_truckstops.py
@@ -5,14 +5,28 @@ def import_truckstops(file_path):
     with open(file_path, 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
-            Truckstop.objects.create(
+
+            existing_truckstop = Truckstop.objects.filter(
                 opis_id=row['OPIS Truckstop ID'],
                 name=row['Truckstop Name'],
                 address=row['Address'],
                 city=row['City'],
-                state=row['State'],
-                rack_id=row['Rack ID'],
-                retail_price=row['Retail Price']
-            )
+                state=row['State']
+            ).first()
+
+            if not existing_truckstop:
+
+                Truckstop.objects.create(
+                    opis_id=row['OPIS Truckstop ID'],
+                    name=row['Truckstop Name'],
+                    address=row['Address'],
+                    city=row['City'],
+                    state=row['State'],
+                    rack_id=row['Rack ID'],
+                    retail_price=row['Retail Price']
+                )
+            else:
+                print(f"Truckstop {row['Truckstop Name']} already exists.")
+
     print("Data imported successfully!")
 
