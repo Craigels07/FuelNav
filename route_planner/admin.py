d2fac56c68d85a309382645a9b56ce660b2a71f5
Craigels <craigh@crs.co.za>
2025-01-16 20:43:21 +0700

    included lat and long fields in dB with duplicate entries removed.

--- route_planner/admin.py
+++ route_planner/admin.py
@@ -3,7 +3,7 @@ from .models import Truckstop
 
 @admin.register(Truckstop)
 class TruckstopAdmin(admin.ModelAdmin):
-    list_display = ('opis_id', 'name', 'address', 'city', 'state', 'rack_id', 'retail_price')
+    list_display = ('opis_id', 'name', 'address', 'city', 'state', 'rack_id', 'retail_price', 'latitude', 'longitude')
     search_fields = ('opis_id', 'name', 'city', 'state')
     list_filter = ('state',)
     ordering = ('opis_id',)
--- route_planner/models.py
+++ route_planner/models.py
@@ -8,6 +8,8 @@ class Truckstop(models.Model):
     state = models.CharField(max_length=2)
     rack_id = models.IntegerField()
     retail_price = models.FloatField()
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.name} ({self.city}, {self.state})"
